adc231d859a10b5ed38526c9006a8f4443761582
sabapatarkalashvili05-commits <saba.patarkalashvili05@gmail.com>
2025-11-16 10:54:52 +0400

    seconde commit

---
+++ day7/classwork/classwork.py
@@ -0,0 +1,62 @@
+#1
+
+#Input – ეს არის შეყვანა. ანუ ინფორმაცია რომელსაც პროგრამას ან მოწყობილობას აძლევ.მაგალითად: კლავიატურაზე Enter-ისდაჭერა ტექსტის წერა რიცხვის შეყვანა.
+
+#Output – ეს არის გამოტანა. ანუ ის ინფორმაცია რასაც მოწყობილობა ან პროგრამა გიბრუნებს input-ის შეტანის შემდეგ.
+
+#2
+age = (input("Enter your age: "))
+print(int(age)+ 5)
+
+product_name=(input("enter you age: "))
+print(int(age)+ 10)
+
+print("Hello World")   # ეს არის output
+print(5 + 5)           # ესეც output (10 გამოიტანს)
+
+
+#3
+
+
+food = input("Enter your favorite food: ")
+print("Your favorite food is:", food)
+
+#4
+
+name=input("Enter your name: ")
+print("hello" , name)
+
+
+#5
+
+num = "10"   
+
+user_num = input("Enter a number: ")
+
+print(int(num) + int(user_num))
+
+#6
+
+#input() აბრუნებს სტრინგს, სტრინგსა და რიცხვს პირდაპირ ვერ აერთიანებ. უნდა გადაიყვანო რიცხვად:
+
+#var = int(input("Enter your age: "))
+#print(var + 3)
+
+
+#7
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("The second number fits in the first", num1 // num2, "times")
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day7/homework/homework.py
@@ -0,0 +1,41 @@
+#1
+age=input("Enter your age:  ")
+
+print(int(age) // 10)
+
+#2
+num1=input("Enter any number  :")
+num2=input("Enter second number  :")
+
+print(int(num1) * int(num2))
+
+#3
+
+your_age=input("Please enter your age  :")
+
+print(int(your_age))
+
+#4
+
+number1=input("Please enter first number  :")
+number2=input("Please enter seccond number  :")
+number3=input("Please enter third number  :")
+
+average = (int(number1 + number2 + number3))/3
+
+print("average is:" , average)
+
+#5
+
+YourAge = int(input("Please enter year you were born: "))
+
+age = 2025 - YourAge
+
+print("Your age is:", age)
+
+
+
+
+
+
+
