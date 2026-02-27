0ade16f07e3581568d663657fe986f6564acdd79
sabapatarkalashvili05-commits <saba.patarkalashvili05@gmail.com>
2026-02-27 21:15:04 +0400

    seconde commit

---
+++ day_21/main.py
@@ -0,0 +1,37 @@
+# 1
+# slicing-გამოიყენება რაიმე ცვლადის ნაწილის გამოსაძახებლად მაგ:
+# name="patarkalashvili"
+# print(name[0:5])
+
+# index-გამოიყენება რაიმე ცვლადის ინდექსის გამოსაძახებლად
+
+
+# 2
+
+name="patarkalashvili"
+print(name[-5:])
+
+
+#  3
+
+list=[1, 45, 3, 6, 8, 7 , 3, 5, 9 ,2]
+
+print(list[:2])
+
+print(list[-4:])
+
+print(list[4:6])
+
+
+# 4
+
+surname=input("please enter your surname:   ")
+
+print(surname[ : :-1])
+
+
+# 5
+
+film="titanic"
+
+print(film[: :2])
